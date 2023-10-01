c5ac308314a38b51cbf4c129bb905ea775cf8dd4
Wilmer <wilmer.maquera@yahoo.com.pe>
2023-10-01 18:22:34 -0500

    conex database

--- clases/conexion/conexion.php
+++ clases/conexion/conexion.php
@@ -37,8 +37,9 @@ class conexion {
     private function datosConexion(){
         $direccion = dirname(__FILE__);
         $jsondata = file_get_contents($direccion . "/" . "config");
-        //return json_decode($jsondata, true);
+        return json_decode($jsondata, true);
 
+        /*
         $DB_HOST = $_ENV["DB_HOST"];
         $DB_USER = $_ENV["DB_USER"];
         $DB_PASSWORD = $_ENV["DB_PASSWORD"];
@@ -47,6 +48,7 @@ class conexion {
 
         $jsondata = '{"DB_HOST":'.$DB_HOST.',"DB_USER":'.$DB_USER.',"DB_PASSWORD":'.$DB_PASSWORD.',"DB_NAME":'.$DB_NAME.',"$DB_PORT":'.$DB_PORT.'}';
         return json_decode($jsondata, true);
+        */
     }
 
 
